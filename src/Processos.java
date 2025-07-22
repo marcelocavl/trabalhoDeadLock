bd8f31456b2b01a44d6fcf5068bc70b2f2c0ed46
davi <davicarvalho.b1@gmail.com>
2025-07-21 23:06:37 -0300

    feat: processos class

---
+++ src/Processos.java
@@ -0,0 +1,18 @@
+//CLASSE PROCESSOS
+public class Processos{
+	public int processo_id; //indentificador do processo
+	public float tempo_solicitacao;
+	public float tempo_utilizacao;
+
+	public void eliminar_processo(int id){
+
+	}
+	public int get_processo_id(){	
+		return this.processo_id;
+	}
+
+	public void set_processo_id(int processo_id){
+		this.processo_id=processo_id;
+	}
+			
+}
