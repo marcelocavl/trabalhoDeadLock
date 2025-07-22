368ad7c4be917b5c4e17766d7645403a6970f247
davi <davicarvalho.b1@gmail.com>
2025-07-21 23:06:29 -0300

    feat: add recursos 1 and 2

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 //IMPORTAÇÕES
 
-import classes.SistemaOperacional;
-
-
-
 //CLASSE MAIN
 public class Main{
 	public static void main(String args[]){	
-		SistemaOperacional so=new SistemaOperacional();
+		SistemaOperacional so = new SistemaOperacional();
+
+		Recursos recurso1 = new Recursos(1, "impressora", 1);
+		Recursos recurso2 = new Recursos(2, "cpu", 1);
 
 		System.out.println(so.get_recursos());
 
-		System.out.println(so.add_recursos("impressora"));
+		System.out.println(so.add_recursos(recurso1));
+		System.out.println(so.add_recursos(recurso2));
 
-		System.out.println(so.get_recursos());
+		System.out.println(so.get_lista_recursos());
 	}	
 }
