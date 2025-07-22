8cf16d11e02e3d7bba315bf7cd64f99b393c494e
davi <davicarvalho.b1@gmail.com>
2025-07-21 23:06:45 -0300

    feat: recursos class

---
+++ src/Recursos.java
@@ -0,0 +1,37 @@
+
+public class Recursos {
+    private final int id;
+    private String nome;
+    private int quantidade;
+
+    public Recursos(int id, String nome, int quantidade){
+        this.id = id;
+        this.nome = nome;
+        this.quantidade = quantidade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Recursos{");
+        sb.append("id=").append(id);
+        sb.append(", nome=").append(nome);
+        sb.append(", quantidade=").append(quantidade);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    
+}
