1d216ffffe7b557782ed64e7363111588f49d08f
marcelocavl <marcelodev1604@gmail.com>
2025-07-23 09:17:03 -0300

    adicionei mais algumas funcoes e comecei a trabalhar mais sobre a classe processo

--- src/classes/Processos.java
+++ src/classes/Processos.java
@@ -1,14 +1,42 @@
 //CLASSE PROCESOS
 public class Processos{
 	public int processo_id; //indetificador do processo
-
+	public ArrayList<integer> recursos_necessarios=new ArrayList<>()
+	//metodos getters and setters
 	public void get_processo_id(){	
 		return self.processo_id;
 	}
 
-	public void set_processo_id(int processo_id){
+	public boolean set_processo_id(int processo_id){
+		if(processo_id==null)
+			return false;
 		self.processo_id=processo_id;
-		return;
+		return true;
 	}
-			
+
+	public void get_recursos_necessarios(){	
+		return self.recursos_necessarios;
+	}
+
+	public boolean set_recursos_necessarios(Arraylist<integer> recursos_necessarios){
+		if(recursos_necessarios==null)
+			return false;
+		self.recursos_necessarios=recursos_necessarios;
+		return true;
+	}
+
+	//metodo de adicionar mais um recurso necessario ao processo
+	public void add_recurso_necessario(int quantidade_de_instancias_necessarias_do_recurso){
+		self.get_recursos_necessarios().add(quantidade_de_instancias_necessarias_do_recurso);
+	}
+	//metodos de verificação
+	//verifica se a quantidade de instancias de um recurso que o processo precisa é superior a quantidade de instancias que o sistema  		operacional possui desse recurso
+	public void is_quantidade_necessaria_daquele_recurso_superior(int quantidade_de_instancias_necessarias_do_recurso){	
+	}
+
+	//verifica se o sistema operacional possui todos os recursos necessarios para o processo
+	public void is_quantidade_necessaria_de_recursos_superior(int quantidade_de_instancias_necessarias_do_recurso){	
+	}
+
+
 }
--- src/classes/SistemaOperacional.java
+++ src/classes/SistemaOperacional.java
@@ -12,7 +12,7 @@ public class SistemaOperacional{
 	public ArrayList<Integer> recursos_quantidade_instancias=new ArrayList<>();
 
 	//METODOS
-	//metodos gets
+	//metodos gets and setters
 	public ArrayList<String> get_recursos(){
 		return this.recursos;
 	}
@@ -23,6 +23,27 @@ public class SistemaOperacional{
 
 	public ArrayList<Integer> get_recursos_quantidade_instancias(){
 		return this.recursos_quantidade_instancias;
+	}
+
+	public boolean set_recursos(ArrayList<String> recursos){
+		if(recursos==null)
+			return false;
+		this.recursos=recursos;
+		return true;
+	}
+
+	public boolean set_recursos_id(ArrayList<Integer> recursos_id){
+		if(recursos_id==null)
+			return false;
+		this.recursos_id=recursos_id;
+		return true;
+	}
+
+	public boolean set_recursos_quantidade_instancias(ArrayList<Integer> recursos_quantidade_instancias){
+		if(recursos_quantidade_instancias==null)
+			return false;
+		this.recursos_quantidade_instancias=recursos_quantidade_instancias;
+		return true;
 	}
 
 	//metodos add
@@ -45,18 +66,37 @@ public class SistemaOperacional{
 		return this.get_recursos().remove(recurso);
 	}
 	
-	//metodos de retorno de indice
-	public int retorna_indice_do_recurso_no_arraylist(String recurso){
+	//metodos de retorno de indice e tamanho do array
+	//retorna o indice do recurso no array recursos
+	public int retorna_indice_do_recurso_no_array_recursos(String recurso){
 		if(!this.is_recurso_existente(recurso))
 			return -1;
 		return this.get_recursos().indexOf(recurso);
 			
 	}
 
+	//retorna o indice do recurso no array recursos_id
+	public int retorna_indice_do_recurso_no_array_recursos_id(int recurso_id){
+		if(!this.is_recurso_existente(recurso_id))
+			return -1;
+		return this.get_recursos_id().indexOf(recurso_id);
+			
+	}
+
+	public int retorna_tamanho_array_recursos(){
+		return this.get_recursos().size();
+	}
+
 	//metodos de verificacao
+	//verifica se o recurso existe no arraylist de recursos
 	public boolean is_recurso_existente(String recurso){
 		return this.get_recursos().contains(recursos);
 	}
+	//verifica se o recurso existe no arraylist de recursos_id
+	public boolean is_recurso_existente(int recurso_id){
+		return this.get_recursos_id().contains(recursos_id);
+	}
+
 
 }
 
