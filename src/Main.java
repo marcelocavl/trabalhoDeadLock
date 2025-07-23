e2095474368aadf5c38ceeb3462429bd7da1ed9c
davi <davicarvalho.b1@gmail.com>
2025-07-23 11:08:51 -0300

    feat: recursos dialog

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,36 @@
+
+import java.util.ArrayList;
+
 //IMPORTAÇÕES
 
 //CLASSE MAIN
+
 public class Main{
 	public static void main(String args[]){	
 		SistemaOperacional so = new SistemaOperacional();
 
-		Recursos recurso1 = new Recursos(1, "impressora", 1);
-		Recursos recurso2 = new Recursos(2, "cpu", 1);
+		SistemaInterface tela = new SistemaInterface();
 
-		System.out.println(so.get_recursos());
+		AddRecursosDialog dialog = new AddRecursosDialog(tela);
+        dialog.setVisible(true);
+			
+        if (dialog.isConfirmed()) {
+			ArrayList<Recursos> recursos = dialog.getRecursos();
 
-		System.out.println(so.add_recursos(recurso1));
-		System.out.println(so.add_recursos(recurso2));
+            // System.out.println("Recurso: " + nome);
+            // System.out.println("ID: " + identificador);
+            // System.out.println("Quantidade: " + quantidade);
 
-		System.out.println(so.get_lista_recursos());
+			so.add_recursos(recursos);
+
+			for (int i = 0; i < recursos.size(); i++){
+				tela.addLog("SO adicionou recurso: " + recursos.get(i).getNome() + " ID (" + recursos.get(i).getId() + ") " + ", quantidade: " + recursos.get(i).getQuantidade());
+				tela.addRecursoRow(recursos.get(i).getNome(),  recursos.get(i).getQuantidade());
+			}
+
+            tela.setVisible(true);
+        } else {
+            System.exit(0);
+        }
 	}	
 }
